08b46d10343d4e9b400fa0c668a0b3153f090dab
raquaza2 <57903323+raquaza2@users.noreply.github.com>
2024-10-13 22:30:35 +0800

    Decryption

--- Encryption.py
+++ Encryption.py
@@ -5,3 +5,13 @@ cipher_text = ""
 for letter in plain_text:
     index = chars.index(letter)
     cipher_text += key[index]
+#DECRYPT
+cipher_text = input("Enter a message to encrypt: ")
+plain_text = ""
+ 
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"encrypted text: {cipher_text}")
+print(f"original text: {plain_text}")
