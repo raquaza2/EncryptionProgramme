12cfe5e56e771148fda4e429ee042d120b7f4e3e
raquaza2 <57903323+raquaza2@users.noreply.github.com>
2024-10-13 22:31:03 +0800

    Final

--- Encryption.py
+++ Encryption.py
@@ -1,3 +1,15 @@
+import random
+import string
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
+
+random.shuffle(key)
+
+#print(f"chars: {chars}")
+#print(f"key: {key}")
+
 #ENCRYPT
 plain_text = input("Enter a message to encrypt: ")
 cipher_text = ""
@@ -5,6 +17,11 @@ cipher_text = ""
 for letter in plain_text:
     index = chars.index(letter)
     cipher_text += key[index]
+  
+print(f"original text: {plain_text}")  
+print(f"encrypted text: {cipher_text}")
+
+
 #DECRYPT
 cipher_text = input("Enter a message to encrypt: ")
 plain_text = ""
